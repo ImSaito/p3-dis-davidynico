bafeac0573ec76c0db63ac896810378a0388c7b9
Saito <davidpv64@gmail.com>
2022-06-08 11:02:49 +0200

    Subimos el main

---
+++ practica2/src/main/java/practica2/practica2/NicoDavidApplication.java
@@ -0,0 +1,16 @@
+package practica2.practica2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+
+public class NicoDavidApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NicoDavidApplication.class);
+	}
+	
+
+}
